adda9e1292548c2fd47ac8c0c7506b91fd6ddcb7
emilynvs <emilynascimento22@outlook.com>
2025-07-22 15:54:23 -0300

    feat: adiciona método listaTudo em LeituraDAO

--- src/main/java/org/example/dao/LeituraDAO.java
+++ src/main/java/org/example/dao/LeituraDAO.java
@@ -17,9 +17,9 @@ public class LeituraDAO {
     ResultSet resultSet;
     LivroDAO dao = new LivroDAO();
 
-    public void  salva(Leitura leitura){
+    public void salva(Leitura leitura) {
         String sql = "INSERT INTO leitura(id_livro, data_inicio, paginas_lidas, nota, comentario, releitura) values(?,?,?,?,?,?)";
-        try{
+        try {
             prepared = connection.prepareStatement(sql);
             prepared.setInt(1, leitura.getIdLivro());
             prepared.setString(2, String.valueOf(leitura.getDataInicio()));
@@ -38,7 +38,7 @@ public class LeituraDAO {
         String sql;
         boolean atualizaPaginas = pag > 0;
 
-        if(atualizaPaginas){
+        if (atualizaPaginas) {
             sql = "UPDATE leitura SET comentario = ?, paginas_lidas = ? WHERE id = ?";
         } else {
             sql = "UPDATE leitura SET comentario = ? WHERE id = ?";
@@ -46,7 +46,7 @@ public class LeituraDAO {
         try {
             prepared = connection.prepareStatement(sql);
             prepared.setString(1, coment);
-            if(atualizaPaginas){
+            if (atualizaPaginas) {
                 prepared.setInt(2, pag);
                 prepared.setInt(3, id);
             } else {
@@ -58,7 +58,7 @@ public class LeituraDAO {
         }
     }
 
-    public void deleta (int id){//recebe o id do histórico de leitura
+    public void deleta(int id) {//recebe o id do histórico de leitura
         try {
             prepared = connection.prepareStatement("DELETE FROM leitura WHERE id = ?");
             prepared.setInt(1, id);
@@ -68,14 +68,14 @@ public class LeituraDAO {
         }
     }
 
-    public Leitura buscaLeitura(int id){
+    public Leitura buscaLeitura(int id) {
 
         Leitura leitura = new Leitura();
         try {
             prepared = connection.prepareStatement("SELECT * FROM leitura WHERE id= ?");
             prepared.setInt(1, id);
             resultSet = prepared.executeQuery();
-            if(resultSet.next()){
+            if (resultSet.next()) {
                 leitura.setComentario(resultSet.getString("comentario"));
                 leitura.setId(resultSet.getInt("id"));
                 leitura.setIdLivro(resultSet.getInt("id_livro"));
@@ -89,14 +89,14 @@ public class LeituraDAO {
         return null;
     }
 
-    public ArrayList<Leitura> exibeHistorico(String titulo){
+    public ArrayList<Leitura> exibeHistorico(String titulo) {
         Livro idLivro = dao.buscaLivro(titulo);
         ArrayList<Leitura> livros = new ArrayList<>();
         try {
             prepared = connection.prepareStatement("SELECT * FROM leitura WHERE id_livro = ?");
             prepared.setString(1, String.valueOf(idLivro.getId()));
             resultSet = prepared.executeQuery();
-            while (resultSet.next()){
+            while (resultSet.next()) {
                 Leitura leitura = new Leitura();
                 leitura.setId(resultSet.getInt("id"));
                 leitura.setIdLivro(resultSet.getInt("id_livro"));
@@ -112,7 +112,27 @@ public class LeituraDAO {
 
     }
 
-    public Leitura listaTudo(){
-        return null;
+    public void listaTudo() {
+        ArrayList<Leitura> leituras = new ArrayList<>();
+        try {
+            prepared = connection.prepareStatement("SELECT * FROM leitura");
+            resultSet = prepared.executeQuery();
+            while (resultSet.next()) {
+                Leitura leitura = new Leitura();
+                leitura.setIdLivro(resultSet.getInt("id_livro"));
+                leitura.setId(resultSet.getInt("id"));
+                leitura.setComentario(resultSet.getString("comentario"));
+                leitura.setDataInicio(LocalDate.parse(resultSet.getString("data_inicio")));
+                leitura.setReleitura(resultSet.getBoolean("releitura"));
+                leitura.setNota(resultSet.getInt("nota"));
+                leitura.setPaginasLidas(resultSet.getInt("paginas_lidas"));
+                leituras.add(leitura);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        for(Leitura leitura: leituras){
+            System.out.println(leitura);
+        }
     }
 }
